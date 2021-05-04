567c9452873dca2c91d8b02cf965edf0595e6037
Alexis Sanchez <psialedev@gmail.com>
2021-05-04 12:16:42 -0500

    Finished basic layout of profile component

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -6,15 +6,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 const Profile = ({handleOnClick}) => (
 
   <div className={styles.mainContainer}>
-   <div className={styles.profileMainContainer}>
+    
      <img src={ProfilePic} alt="Alexis Sanchez"/>
-     <div className={styles.profileInfoContainer}>
-       <p>Alexis Sanchez</p>
+       <h2>Alexis Sanchez</h2>
        <p>Fullstack Developer</p>
        <p onClick={handleOnClick}>psialedev@gmail.com</p>
-     </div>
-   </div>
-   <div>
+   <div className={styles.socialMediaContainer}>
     <a href="https://www.linkedin.com/in/alexis-sanchez-dev/">
     <FontAwesomeIcon icon={['fab', 'linkedin']} />
     </a>
