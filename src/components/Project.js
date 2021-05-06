b1e12622e5593aa2ce48a2a81863666f671e1668
Alexis Sanchez <psialedev@gmail.com>
2021-05-06 11:31:44 -0500

    Restored Label to use it on mobile view

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,17 +4,17 @@ import styles from './Project.module.css';
 const Project = ({title, desktopImg, mobileImg, details, technologies, urls}) => {
     return (
       <>
-        <p>{ title }</p>
-          <div className={styles.imagesContainer}>
+        <p style={{textAlign: urls.isMobile ? 'center' : 'initial'}}>{ title }</p>
+          <div style={{flexDirection: urls.isMobile ? 'row' : 'column', justifyContent: urls.isMobile ? 'center' : 'center' }} className={styles.imagesContainer}>
            {(urls.isMobile) ? <img
            style={{
               maxWidth: '200px', 
               minWidth: 'unset',
-              maxHeight: 'unset',
+              maxHeight: '427px',
               minHeight: 'unset',
               width: '50%',
               height: '100%'}} src={desktopImg} alt="desktop project"/> : <img className={styles.desktopImg} src={desktopImg} alt="desktop project"/> } {(mobileImg !== undefined) ? <img className={styles.mobileImg} src={mobileImg} alt=" mobile project"/> : null}
-            <ul className={styles.technologiesContainer}>
+            <ul style={{flexDirection: urls.isMobile ? 'column' : 'row', alignItems:  urls.isMobile ? 'flex-start' : 'center' }} className={styles.technologiesContainer}>
             {technologies.map(technology => (<> <li>{technology.title}</li> </>))}
           </ul>
         </div>
